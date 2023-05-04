fb3162b13e5e5d008126aba141d7718dabe98e00
zeynepulas <zeynepulas2000@gmail.com>
2023-05-04 16:52:01 +0300

    update
    fixed

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-const num1 = Math.ceil(Math.random()*10)
-const num2 = Math.ceil(Math.random()*10)
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
 const questionEl = document.getElementById("question");
-const inputEl = document.getElementById("input")
-const fromEl = document.getElementById("form")
-const scoreEl = document.getElementById("score")
+
+const inputEl = document.getElementById("input");
+
+const formEl = document.getElementById("form");
+
+const scoreEl = document.getElementById("score");
+
 let score = JSON.parse(localStorage.getItem("score"));
-if(!score){
-    score = 0;
+
+if (!score) {
+  score = 0;
 }
-scoreEl.innerText = "score: "+score;
-questionEl.innerText = "What is "+num1+" multiplied by " +num2+"?"
-const correctAnswer = num1 * num2
-fromEl.addEventListener("sub", ()=>{
-    const userAnswer = inputEl.value
-    if(userAnswer === correctAnswer){
-        score++
-    }else{
-        score--
-    }
+
+scoreEl.innerText = `score: ${score}`;
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+  const userAns = +inputEl.value;
+  if (userAns === correctAns) {
+    score++;
+    updateLocalStorage();
+  } else {
+    score--;
+    updateLocalStorage();
+  }
 });
-function UpdatLocalStorage(){
-    localStorage.setItem("score", JSON.stringify(score))
+
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score));
 }
